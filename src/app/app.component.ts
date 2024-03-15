86096b2c578929743d18549c88dd13c18f38c0cd
toobi <grzechu16789@gmail.com>
2024-03-15 11:45:30 +0100

    Created title component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleComponent } from './title/title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/title/title.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'cropp-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './title.component.html',
+  styleUrl: './title.component.scss'
+})
+export class TitleComponent {
+  @Input({ required: true }) title: string = '';
+}
