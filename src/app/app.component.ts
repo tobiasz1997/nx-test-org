879d52ac4db3b0b6caf96fc34854306534a19981
toobi <grzechu16789@gmail.com>
2024-03-17 19:41:52 +0100

    Added link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TitleComponent } from './title/title.component';
+import { LinkComponent } from './link/link.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleComponent],
+  imports: [RouterOutlet, TitleComponent, LinkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
